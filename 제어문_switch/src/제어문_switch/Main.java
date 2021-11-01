ad2a9672b74aee2e62e1520ac0d9c3aae31d6d8b
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2021-11-01 23:32:51 +0900

    계절 판별하기

---
+++ 제어문_switch/src/제어문_switch/Main.java
@@ -0,0 +1,40 @@
+package 제어문_switch;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int month = scanner.nextInt();
+		
+		switch (month) {
+		case 3:
+		case 4:
+		case 5:
+			System.out.println("봄");
+			break;
+		
+		case 6:
+		case 7:
+		case 8:
+			System.out.println("여름");
+			break;
+			
+		case 9:	
+		case 10:	
+		case 11:
+			System.out.println("가을");
+			break;
+			
+		case 12:
+		case 1:
+		case 2:
+			System.out.println("겨울");
+			break;
+
+		default:
+			break;
+		}
+	}
+}
